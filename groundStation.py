3025353c519e1553561b162e91736dbdac9359a0
Levi Willmeth <lo9key@gmail.com>
2015-07-26 17:17:10 -0700

    Began preparing to use pygmaps, which takes decimal degree input instead of strings.  Created dmsToDecimal() function to fix formatting, but no tests completed yet.

--- groundStation.py
+++ groundStation.py
@@ -5,6 +5,24 @@ import time, serial
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=3)
 print ser
 
+def dmsToDecimal(deg, minSec, hemisphere):
+    '''
+        Converts from degrees, minutes, seconds to decimal degrees.
+    '''
+    min, sec = minSec.split('.')
+    try:
+        deg = int(deg)
+        min = int(min)
+        sec = int(sec)
+    except Exception, e:
+        print "Malformed GPS coordinates: " + str(e)
+        
+    result = deg + (min*60+sec) / 3600
+    # Account for pos/neg degrees
+    if hemisphere == 'W' or hemisphere == 'S':
+        result *= -1
+    return result
+
 def parseGPS(line):
     '''
     Parse a gps string into a list of components.
@@ -13,12 +31,12 @@ def parseGPS(line):
     '''
     try:
         # lat should be in format 'dd mm.ss N'
-        lat = line[1:3] + line[3:9]
+        lat = line[1:3] + ' ' + line[3:9]
         # ugly fix for spacing out the N char
         lat = lat[:len(lat)-1]+' '+lat[-1:]
 
         # long should be in format 'ddd mm.ss W'
-        long = line[10:13] + line[12:19]
+        long = line[10:13] + ' ' + line[13:19]
         long = long[:len(long)-1]+' '+long[-1:]
 
         alt, volt = line[30:36], line[37:40]
@@ -27,12 +45,23 @@ def parseGPS(line):
         print "Error: " + str(e)
 
 while True:
+    '''
+    Each gps reading is made up of two seperate lines:
+        N7SEC-1>APBL10,WIDE1-1,WIDE2-1: <<UI>>:
+        !4432.21N/12315.39WO000/000/A=000099V2F6LBCC Near-Space Exploration
+
+    The first line gives us the callsign and information about the ground
+    station, the second line gives us information about the payload location.
+    '''
     data = ser.readline()
     if data:
         callsign = data.split('>')[0]
         if callsign == 'N7SEC-1':
             lat, long, alt, volt = parseGPS(ser.readline())
+            latF = dmsToDecimal(lat)
+            longF = dmsToDecimal(long)
             print 'GPS Coordinates: {}, {}'.format(lat, long)
+            print 'Converted as: {}, {}'.format(latF, longF)
             print 'Altitude: {}, voltage:{}'.format(alt, volt)
         else:
             print "Invalid callsign: " + data
